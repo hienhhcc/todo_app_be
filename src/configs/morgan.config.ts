cceddf4f570227351872c3bd1f900c451c55f10f
hienhhcc <thachdau16t@gmail.com>
2022-02-20 18:50:47 +0700

    Update morgan.config.ts

--- src/configs/morgan.config.ts
+++ src/configs/morgan.config.ts
@@ -1,19 +1,19 @@
-import morgan from 'morgan';
-import { env } from './config';
-import logger from './logger.config';
+// import morgan from 'morgan';
+// import { env } from './config';
+// import logger from './logger.config';
 
-morgan.token('message', (req: any, res: any) => res.locals.errorMessage || '');
+// morgan.token('message', (req: any, res: any) => res.locals.errorMessage || '');
 
-const getIpFormat = () => (env === 'production' ? ':remote-addr - ' : '');
-const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
-const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
+// const getIpFormat = () => (env === 'production' ? ':remote-addr - ' : '');
+// const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
+// const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
 
-export const successHandler = morgan(successResponseFormat, {
-  skip: (req: any, res: any) => res.statusCode >= 400,
-  stream: { write: (message: any) => logger.info(message.trim()) },
-});
+// export const successHandler = morgan(successResponseFormat, {
+//   skip: (req: any, res: any) => res.statusCode >= 400,
+//   stream: { write: (message: any) => logger.info(message.trim()) },
+// });
 
-export const errorHandler = morgan(errorResponseFormat, {
-  skip: (req: any, res: any) => res.statusCode < 400,
-  stream: { write: (message: any) => logger.error(message.trim()) },
-});
+// export const errorHandler = morgan(errorResponseFormat, {
+//   skip: (req: any, res: any) => res.statusCode < 400,
+//   stream: { write: (message: any) => logger.error(message.trim()) },
+// });
